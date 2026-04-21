06f850bbc37447f40cc2068e8a7f04468b31d04e
Eddie Hill <550046@escg.ac.uk>
2026-04-21 16:12:29 +0100

    tennis progress

---
+++ Assets/Scripts/Minigame/MGs/02_Tennis.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class _02_Tennis : MGManager
+{
+    Rigidbody
+    public override void MGStart()
+    {
+        
+    }
+
+    public override void MGUpdate()
+    {
+        
+    }
+}
