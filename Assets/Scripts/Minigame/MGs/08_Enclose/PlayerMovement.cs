f9950704424b319f397e268b85361053e876f28e
Eddie Hill <550046@escg.ac.uk>
2026-05-26 12:28:20 +0100

    add comments

--- Assets/Scripts/Minigame/MGs/08_Enclose/PlayerMovement.cs
+++ Assets/Scripts/Minigame/MGs/08_Enclose/PlayerMovement.cs
@@ -2,30 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// VERY simple player class
+
 public class PlayerMovement : BB.PhysicsObject
 {
     // Start is called before the first frame update
     public override void ActorStart()
     {
+        // uh
     }
     static float speed = 7.5f;
     static float jumpspeed = 20.0f;
     // Update is called once per frame
     public override void ActorUpdate()
     {
+        // left movement
         if (Input.GetKey(KeyCode.LeftArrow) && !isLeft)
         {
             rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
         }
+        // right movement
         else if (Input.GetKey(KeyCode.RightArrow) && !isRight)
         {
             rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
         }
+        // no movement
         else
         {
             rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
         }
 
+        // jumping!
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpspeed);
