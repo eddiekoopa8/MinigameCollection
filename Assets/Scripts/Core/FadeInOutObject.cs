c59432fa5bca29d6a807cac6abde54ec923be177
Eddie Hill <550046@escg.ac.uk>
2026-05-26 12:03:27 +0100

    add comments

--- Assets/Scripts/Core/FadeInOutObject.cs
+++ Assets/Scripts/Core/FadeInOutObject.cs
@@ -7,6 +7,7 @@ namespace Core
 {
     public class FadeInOutObject : FadeObject
     {
+        // Fade states
         enum Fade
         {
             IDLE,
@@ -14,6 +15,7 @@ namespace Core
             OUT
         } Fade state = Fade.IDLE;
 
+        // Default speed
         float speed = 1.0f;
 
         public void FadeIn(float FadeSpeed = 1.0f)
@@ -28,6 +30,7 @@ namespace Core
             speed = FadeSpeed;
         }
 
+        // Read only booleans for public use.
         public bool FadedIn { get { return FadeAlpha >= 255; } }
         public bool FadedOut { get { return FadeAlpha <= 0; } }
         public bool Faded { get { return FadedIn || FadedOut; } }
@@ -36,28 +39,40 @@ namespace Core
         {
             switch (state)
             {
+                // Fading in
                 case Fade.IN:
                     {
+                        // If we haven't reached to alpha 255
                         if (FadeAlpha < 255)
                         {
+                            // Keep on fading in! (based on delta time)
                             FadeAlpha += Time.deltaTime * (100 * speed);
                         }
+                        // If we have reached to alpha 255
                         else
                         {
+                            // We faded in! Limit it to 255.
                             FadeAlpha = 255;
+                            // Wait for another fade in/out request
                             state = Fade.IDLE;
                         }
                         break;
                     }
+                // Fading out
                 case Fade.OUT:
                     {
+                        // If we haven't reached to alpha 0
                         if (FadeAlpha > 0)
                         {
+                            // Keep on fading out! (based on delta time)
                             FadeAlpha -= Time.deltaTime * (100 * speed);
                         }
+                        // If we have reached to alpha 0
                         else
                         {
+                            // We faded out! Wait for another fade in/out request
                             state = Fade.IDLE;
+                            // Limit alpha to 0
                             FadeAlpha = 0;
                         }
                         break;
