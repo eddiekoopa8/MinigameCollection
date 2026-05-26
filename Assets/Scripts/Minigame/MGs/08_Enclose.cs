1bb7fc52c8aeecf26b2ed4dad9e93ae01915a4ee
Eddie Hill <550046@escg.ac.uk>
2026-05-26 14:04:34 +0100

    add comments

--- Assets/Scripts/Minigame/MGs/08_Enclose.cs
+++ Assets/Scripts/Minigame/MGs/08_Enclose.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// the rest is in the exclusive script folder
+
 public class _08_Enclose : MGManager
 {
     SimpleCollisionListener player;
@@ -15,6 +17,7 @@ public class _08_Enclose : MGManager
 
     public override void MGUpdate()
     {
+        // if player touches ceiling, lose
         if (player.Has("Death"))
         {
             Destroy(player.gameObject);
@@ -22,6 +25,7 @@ public class _08_Enclose : MGManager
             LostEndMG();
         }
 
+        // Move ceiling
         if (death.transform.position.y > 0)
             death.transform.SetPositionAndRotation(new Vector3(death.transform.position.x, death.transform.position.y - (Time.deltaTime * 4.5f), death.transform.position.z), death.transform.rotation);
         if (death.transform.position.y <= 0)
